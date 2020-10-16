0a46b3a030baed7107d64e705c7516fd3e474f37
yluego <vcrod79@gmail.com>
2020-10-15 22:04:06 -0700

    4.2.6

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,27 @@ tasksToDoEl.appendChild(listItemEl);
 var createTaskHandler = function (event) {
 
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  console.log(taskTypeInput);
 
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
+
+  listItemEl.textContent = taskNameInput;
   tasksToDoEl.appendChild(listItemEl);
 
 }
